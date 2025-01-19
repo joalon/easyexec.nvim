e4d28b86430751ffb0879f69155faa051ac8687e
Joakim Lönnegren <joakimlonnegren@gmail.com>
2025-01-19 08:56:36 +0100

    Make the buffer scroll to the end when a command is executed

--- lua/easyexec/init.lua
+++ lua/easyexec/init.lua
@@ -4,6 +4,24 @@ M.current_channel_id = nil
 M.current_buffer = -1
 M.last_command = nil
 
+-- Following function borrowed from: https://github.com/ViRu-ThE-ViRuS/configs/blob/f2b001b07b0da4c39b3beea00c90f249906d375c/nvim/lua/lib/misc.lua#L27
+-- scroll target buffer to end (set cursor to last line)
+local function scroll_to_end(bufnr)
+	local cur_win = vim.api.nvim_get_current_win()
+
+	-- switch to buf and set cursor
+	vim.api.nvim_buf_call(bufnr, function()
+		local target_win = vim.api.nvim_get_current_win()
+		vim.api.nvim_set_current_win(target_win)
+
+		local target_line = vim.tbl_count(vim.api.nvim_buf_get_lines(0, 0, -1, true))
+		vim.api.nvim_win_set_cursor(target_win, { target_line, 0 })
+	end)
+
+	-- return to original window
+	vim.api.nvim_set_current_win(cur_win)
+end
+
 function M.exec()
 	local command = vim.fn.input({ prompt = "Exec: ", default = M.last_command })
 
@@ -35,6 +53,7 @@ function M.exec()
 
 	vim.fn.chansend(M.current_channel_id, { command, "" })
 	M.last_command = command
+	scroll_to_end(M.current_buffer)
 end
 
 return M
