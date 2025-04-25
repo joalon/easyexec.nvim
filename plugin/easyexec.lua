75e97ef9c319502737eede37986acce8ada839dd
Joakim Lönnegren <joakimlonnegren@gmail.com>
2025-04-25 07:24:27 +0200

    Add a <Plug> keymap and a plugin load guard

--- plugin/easyexec.lua
+++ plugin/easyexec.lua
@@ -1,3 +1,12 @@
+if vim.g.loaded_easyexec == 1 then
+	return
+end
+vim.g.loaded_easyexec = 1
+
+vim.keymap.set("n", "<Plug>(Easyexec)", function()
+	require("easyexec").exec()
+end, { noremap = true })
+
 vim.api.nvim_create_user_command("Easyexec", function()
 	require("easyexec").exec()
 end, {
